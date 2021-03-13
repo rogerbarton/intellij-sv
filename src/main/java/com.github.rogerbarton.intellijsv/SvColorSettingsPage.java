e17470fef58a75c95b2faf76546aa7daee49b565
Roger Barton <rbarton@student.ethz.ch>
2021-03-13 13:51:21 +0100

    Add syntax highlighter + color settings page

---
+++ src/main/java/com.github.rogerbarton.intellijsv/SvColorSettingsPage.java
@@ -0,0 +1,68 @@
+package com.github.rogerbarton.intellijsv;
+
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class SvColorSettingsPage implements ColorSettingsPage {
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[] {
+            new AttributesDescriptor("Key", SvSyntaxHighlighter.KEY),
+            new AttributesDescriptor("Separator", SvSyntaxHighlighter.SEPARATOR),
+            new AttributesDescriptor("Value", SvSyntaxHighlighter.VALUE),
+            new AttributesDescriptor("Bad Value", SvSyntaxHighlighter.BAD_CHARACTER),
+    };
+
+    @Override
+    public @Nullable Icon getIcon() {
+        return SvIcons.FILE;
+    }
+
+    @Override
+    public @NotNull SyntaxHighlighter getHighlighter() {
+        return new SvSyntaxHighlighter();
+    }
+
+    @Override
+    public @NotNull String getDemoText() {
+        return "# You are reading the \".properties\" entry.\n" +
+                "! The exclamation mark can also mark text as comments.\n" +
+                "website = https://en.wikipedia.org/\n" +
+                "language = English\n" +
+                "# The backslash below tells the application to continue reading\n" +
+                "# the value onto the next line.\n" +
+                "message = Welcome to \\\n" +
+                "          Wikipedia!\n" +
+                "# Add spaces to the key\n" +
+                "key\\ with\\ spaces = This is the value that could be looked up with the key \"key with spaces\".\n" +
+                "# Unicode\n" +
+                "tab : \\u0009";
+    }
+
+    @Override
+    public @Nullable Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return null;
+    }
+
+    @Override
+    public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @Override
+    public ColorDescriptor @NotNull [] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @Override
+    public @NotNull String getDisplayName() {
+        return "System Verilog";
+    }
+}

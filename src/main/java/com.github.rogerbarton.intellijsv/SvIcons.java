08df0994fcaa7bb2ac37e5dbb67ae018c470513f
Roger Barton <rbarton@student.ethz.ch>
2021-03-13 11:07:15 +0100

    Sv filetype

---
+++ src/main/java/com.github.rogerbarton.intellijsv/SvIcons.java
@@ -0,0 +1,9 @@
+package com.github.rogerbarton.intellijsv;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class SvIcons {
+    public static final Icon FILE = IconLoader.getIcon("/icons/sv-file.png");
+}

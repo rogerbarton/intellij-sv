aaba4d4218b058558d5aebce3517adb3827a01d9
Roger Barton <rbarton@student.ethz.ch>
2021-07-06 18:23:30 +0200

    Move test files to resources

--- src/test/kotlin/ch/rbarton/core/parser/TimedefParsingTest.kt
+++ src/test/kotlin/ch/rbarton/core/parser/TimedefParsingTest.kt
@@ -3,7 +3,7 @@ package ch.rbarton.core.parser
 import ch.rbarton.intellijsv.core.psi.SvParserDefinition
 import com.intellij.testFramework.ParsingTestCase;
 
-class TimedefParsingTest : ParsingTestCase("src/test/testData", "sv", SvParserDefinition())
+class TimedefParsingTest : ParsingTestCase("src/test/resources", "sv", SvParserDefinition())
 {
     override fun skipSpaces() = true
 

178347838aced2076cd56bf9002d99991ead6110
Roger Barton <rbarton@student.ethz.ch>
2021-07-15 13:27:00 +0200

    SvParserDefinition

--- src/main/kotlin/ch/rbarton/intellijsv/core/psi/SvParserDefinition.kt
+++ src/main/kotlin/ch/rbarton/intellijsv/core/psi/SvParserDefinition.kt
@@ -4,8 +4,10 @@ import ch.rbarton.intellijsv.SvLanguage
 import ch.rbarton.intellijsv.core.lexer.SvLexer
 import com.intellij.lang.ParserDefinition
 import ch.rbarton.intellijsv.core.parser.SvParser
+import ch.rbarton.intellijsv.core.psi.SvTypes.*
 import com.intellij.psi.tree.TokenSet
 import com.intellij.lang.ASTNode
+import com.intellij.lang.LanguageUtil
 import com.intellij.lang.PsiParser
 import com.intellij.psi.tree.IFileElementType
 import com.intellij.psi.FileViewProvider
@@ -19,28 +21,28 @@ import com.intellij.psi.TokenType
 class SvParserDefinition : ParserDefinition
 {
     override fun createLexer(project: Project): Lexer = SvLexer()
+    override fun createParser(project: Project): PsiParser = SvParser()
+    override fun createFile(viewProvider: FileViewProvider): PsiFile = SvFile(viewProvider)
 
-    override fun getWhitespaceTokens(): TokenSet = WHITE_SPACES
-
-    override fun getCommentTokens(): TokenSet = COMMENTS
-
-    override fun getStringLiteralElements(): TokenSet = TokenSet.EMPTY
-
-    override fun createParser(project: Project): PsiParser =
-        SvParser()
+    override fun getWhitespaceTokens(): TokenSet = TokenSet.create(TokenType.WHITE_SPACE)
+    override fun getCommentTokens(): TokenSet = SV_COMMENTS
+    override fun getStringLiteralElements(): TokenSet = SV_STRING_LITERALS
 
     override fun getFileNodeType(): IFileElementType = FILE
 
-    override fun createFile(viewProvider: FileViewProvider): PsiFile = SvFile(viewProvider)
+    override fun spaceExistenceTypeBetweenTokens(left: ASTNode, right: ASTNode): SpaceRequirements
+    {
+        // Make Lexer check if a space is required or not
+        return if (left.elementType == LINE_COMMENT)
+            SpaceRequirements.MUST_LINE_BREAK
+        else
+            LanguageUtil.canStickTokensTogetherByLexer(left, right, SvLexer())
+    }
 
-    override fun spaceExistenceTypeBetweenTokens(left: ASTNode, right: ASTNode) = SpaceRequirements.MAY
-
-    override fun createElement(node: ASTNode): PsiElement = SvTypes.Factory.createElement(node)
+    override fun createElement(node: ASTNode): PsiElement = Factory.createElement(node)
 
     companion object
     {
         val FILE: IFileElementType = IFileElementType(SvLanguage);
-        val WHITE_SPACES = TokenSet.create(TokenType.WHITE_SPACE)
-        val COMMENTS = TokenSet.create(SvTypes.LINE_COMMENT)
     }
 }

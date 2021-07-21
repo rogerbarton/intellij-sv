81e3281198d6d4248dbc623aff7db86863325dcd
Roger Barton <rbarton@student.ethz.ch>
2021-07-21 12:45:16 +0200

    Net and Port reference resolution

--- src/main/kotlin/ch/rbarton/intellijsv/core/psi/SvUtil.kt
+++ src/main/kotlin/ch/rbarton/intellijsv/core/psi/SvUtil.kt
@@ -3,6 +3,7 @@ package ch.rbarton.intellijsv.core.psi
 import ch.rbarton.intellijsv.SvFileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
@@ -10,22 +11,57 @@ import com.intellij.psi.util.PsiTreeUtil
 
 class SvUtil
 {
-    companion object {
-        fun findModuleIdentifiers(project: Project, id: String? = null): List<SvModuleDeclaration>
+    companion object
+    {
+        fun findModuleIdentifiers(project: Project, query: String? = null): List<SvModuleDeclaration>
         {
             val result: MutableList<SvModuleDeclaration> = mutableListOf()
-            val virtualFiles: Collection<VirtualFile> = FileTypeIndex.getFiles(SvFileType.INSTANCE, GlobalSearchScope.allScope(project))
+            val virtualFiles: Collection<VirtualFile> =
+                FileTypeIndex.getFiles(SvFileType.INSTANCE, GlobalSearchScope.allScope(project))
 
-            for(virtualFile: VirtualFile in virtualFiles)
+            for (virtualFile: VirtualFile in virtualFiles)
             {
                 val svFile: SvFile = PsiManager.getInstance(project).findFile(virtualFile) as SvFile
-                val moduleDecls: Array<SvModuleDeclaration> = PsiTreeUtil.getChildrenOfType(svFile, SvModuleDeclaration::class.java) ?: continue
+                val moduleDecls: Array<SvModuleDeclaration> =
+                    PsiTreeUtil.getChildrenOfType(svFile, SvModuleDeclaration::class.java) ?: continue
 
-                if (id == null)
+                if (query == null)
                     result += moduleDecls
                 else
-                    result += moduleDecls.filter { it.identifier.text.equals(id) }.toList()
+                    result += moduleDecls.filter { it.identifier.text.equals(query) }.toList()
             }
+            return result
+        }
+
+        fun findPortIdentifiers(containingModule: SvModuleDeclaration, query: String? = null): List<SvPortDeclaration>
+        {
+            if (containingModule.moduleHeader == null) return emptyList()
+
+            return containingModule.moduleHeader!!.portDeclarationList.filter {
+                it.identifier?.text.equals(query)
+            }
+        }
+
+        fun findNetIdentifiers(containingModule: SvModuleDeclaration, query: String? = null): List<Pair<SvNetDeclaration, PsiElement>>
+        {
+            val result: MutableList<Pair<SvNetDeclaration, PsiElement>> = mutableListOf()
+
+            // Get NetDeclarations with a valid containing identifier, note: it may have multiple
+            val validNetDecls: List<SvNetDeclaration> =
+                containingModule.moduleItemList.filter {
+                    it.netDeclaration != null
+                        && it.netDeclaration!!.identifierWithDefaultList.any { idd -> idd.identifier.text.equals(query) }
+                }.map { it.netDeclaration!! }
+
+            // Extract NetDecl and Identifier pairs
+            for (decl in validNetDecls) {
+                for(identifierWithDefault in decl.identifierWithDefaultList)
+                {
+                    if(identifierWithDefault.identifier.text.equals(query))
+                        result += Pair(decl, identifierWithDefault.identifier)
+                }
+            }
+
             return result
         }
     }
---
+++ src/main/kotlin/ch/rbarton/intellijsv/core/resolve/impl/SvNetReferenceImpl.kt
@@ -0,0 +1,57 @@
+package ch.rbarton.intellijsv.core.resolve.impl
+
+import ch.rbarton.intellijsv.core.SvIcons
+import ch.rbarton.intellijsv.core.psi.SvModuleDeclaration
+import ch.rbarton.intellijsv.core.psi.SvNetDeclaration
+import ch.rbarton.intellijsv.core.psi.SvPortDeclaration
+import ch.rbarton.intellijsv.core.psi.SvUtil
+import ch.rbarton.intellijsv.core.psi.ext.SvReferenceElement
+import ch.rbarton.intellijsv.core.resolve.SvPolyReference
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementResolveResult
+import com.intellij.psi.ResolveResult
+import com.intellij.psi.util.PsiTreeUtil
+
+/**
+ * Defines how to resolve a reference to a net declaration.
+ * Searches in module ports and containing net declarations.
+ */
+class SvNetReferenceImpl(element: SvReferenceElement) : SvPolyReference<SvReferenceElement>(element)
+{
+    override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult>
+    {
+        if (myElement?.referenceElement == null) return ResolveResult.EMPTY_ARRAY
+        val queryId: String = myElement.referenceElement!!.text
+
+        val parentModule: SvModuleDeclaration = PsiTreeUtil.getParentOfType(myElement, SvModuleDeclaration::class.java)
+            ?: return ResolveResult.EMPTY_ARRAY
+
+        val portDecls =
+            SvUtil.findPortIdentifiers(parentModule, queryId).map { PsiElementResolveResult(it.identifier!!) }
+        val netDecls = SvUtil.findNetIdentifiers(parentModule, queryId).map { PsiElementResolveResult(it.second) }
+
+        return (portDecls + netDecls).toTypedArray()
+    }
+
+    override fun getVariants(): Array<out LookupElement>
+    {
+        val variants: MutableList<LookupElement> = mutableListOf()
+
+        val parentModule: SvModuleDeclaration = PsiTreeUtil.getParentOfType(myElement, SvModuleDeclaration::class.java)
+            ?: return LookupElement.EMPTY_ARRAY
+
+        SvUtil.findPortIdentifiers(parentModule, null).forEach {
+            variants += LookupElementBuilder.create(it).withIcon(SvIcons.SV_NET)
+        }
+        SvUtil.findNetIdentifiers(parentModule, null).forEach {
+            variants += LookupElementBuilder.create(it).withIcon(SvIcons.SV_NET)
+        }
+
+        return variants.toTypedArray()
+    }
+
+    override fun isReferenceTo(element: PsiElement): Boolean =
+        (element is SvNetDeclaration || element is SvPortDeclaration) && super.isReferenceTo(element)
+}
